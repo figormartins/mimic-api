594839478fa9eaaa4d8e9259e69798ac17e9fce0
Igor Martins <figortmartins@gmail.com>
2020-07-14 20:58:51 -0300

    Added WordRepository to project

---
+++ MimicApi/Repositories/Contracts/IWordRepository.cs
@@ -0,0 +1,18 @@
+﻿using MimicApi.Helpers;
+using MimicApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MimicApi.Repositories.Contracts
+{
+    public interface IWordRepository
+    {
+        IEnumerable<Word> GetWords(WordUrlQuery query);
+        Word GetWord(int id);
+        void PostWord(Word word);
+        void UpdateWord(Word word);
+        void DeleteWord(int id);
+    }
+}
---
+++ MimicApi/Repositories/WordRepository.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MimicApi.Data;
+using MimicApi.Helpers;
+using MimicApi.Models;
+using MimicApi.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MimicApi.Repositories
+{
+    public class WordRepository : IWordRepository
+    {
+        private readonly MimicContext _context;
+
+        public WordRepository(MimicContext context)
+        {
+            _context = context;
+        }
+
+
+        public IEnumerable<Word> GetWords(WordUrlQuery query)
+        {
+            var words = _context.Words
+                .AsQueryable();
+
+            if (query.Date.HasValue)
+            {
+                words = words
+                    .Where(w => w.CreatedAt > query.Date.Value || w.ModifiedAt > query.Date.Value);
+            }
+
+            if (query.Page.HasValue && query.Quantity.HasValue)
+            {
+                var totalQuantity = words.Count();
+                words = words
+                    .Skip((query.Page.Value - 1) * query.Quantity.Value)
+                    .Take(query.Quantity.Value);
+
+                var pagination = new Pagination()
+                {
+                    Page = query.Page.Value,
+                    Quantity = query.Quantity.Value,
+                    TotalItems = totalQuantity,
+                    TotalPages = (int)Math.Ceiling((double)totalQuantity / query.Quantity.Value)
+                };
+            }
+
+            return words;
+        }
+
+        public Word GetWord(int id)
+        {
+            return _context.Words
+                .AsNoTracking()
+                .FirstOrDefault(w => w.Id == id);
+        }
+
+        public void PostWord(Word word)
+        {
+            _context.Words.Add(word);
+            _context.SaveChanges();
+        }
+
+        public void UpdateWord(Word word)
+        {
+            _context.Words.Update(word);
+            _context.SaveChanges();
+        }
+
+        public void DeleteWord(int id)
+        {
+            var word = GetWord(id);
+            _context.Words.Update(word);
+            _context.SaveChanges();
+        }
+    }
+}
