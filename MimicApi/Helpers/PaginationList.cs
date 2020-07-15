a2e1d3fb5ef909603c5259104c4798faa3676e57
Igor Martins <figortmartins@gmail.com>
2020-07-15 20:40:50 -0300

    Added PaginationList

---
+++ MimicApi/Helpers/PaginationList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace MimicApi.Helpers
+{
+    public class PaginationList<T> : List<T>
+    {
+        public Pagination Pagination { get; set; }
+    }
+}
