7dbd6973d84bc55b89c31479adea0f51fbcc7405
Igor Martins <figortmartins@gmail.com>
2020-07-20 17:46:43 -0300

    DTPMapper and mapping word

--- MimicApi/Helpers/DTOMapperPrifile.cs
+++ MimicApi/Helpers/DTOMapperPrifile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using MimicApi.Models;
+using MimicApi.Models.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,9 @@ namespace MimicApi.Helpers
 {
     public class DTOMapperPrifile : Profile
     {
+        public DTOMapperPrifile()
+        {
+            CreateMap<Word, WordDTO>();
+        }
     }
 }
